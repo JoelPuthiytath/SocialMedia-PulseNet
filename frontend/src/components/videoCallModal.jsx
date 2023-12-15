9469b961ec7a8f33e39ff9eb1bcb5ecd54429a4d
joel baby <joelbabyputhiyath@gmail.com>
2023-12-15 12:19:23 +0530

    videoCall updation

--- frontend/src/components/videoCallModal.jsx
+++ frontend/src/components/videoCallModal.jsx
@@ -1,14 +1,15 @@
 import Modal from "@mui/material/Modal";
 import { styled } from "@mui/system";
 import CallEndIcon from "@mui/icons-material/CallEnd";
-import { useEffect, useRef, useState, useLayoutEffect } from "react";
+import PhoneIcon from "@mui/icons-material/Phone";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 // import global from "global";
 // import * as process from "process";
 // global.process = process;
 // import Peer from "peerjs";
-import { Button } from "@mui/material";
-
+import { Button, IconButton } from "@mui/material";
+// import "./videoCallModel.css";
 const StyledModal = styled(Modal)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
@@ -21,11 +22,52 @@ const ModalContent = styled("div")(({ theme }) => ({
   boxShadow: theme.shadows[5],
   padding: theme.spacing(2, 4, 3),
   display: "flex",
+  position: "relatiive",
   flexDirection: "column",
   alignItems: "center",
   textAlign: "center",
+  height: "80vh",
+  maxWidth: "500px", // Adjust the modal width
 }));
 
+const VideoContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginBottom: theme.spacing(2),
+}));
+
+const Video = styled("video")(({ theme }) => ({
+  width: "100%",
+  maxWidth: "400px",
+  borderRadius: "8px",
+  marginBottom: theme.spacing(1),
+}));
+
+const CallerInfo = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  color: "white",
+  transform: " translateY(-160%)",
+
+  marginTop: theme.spacing(2),
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+  width: "100%",
+}));
+
+const ButtonContainer = styled("div")(({ theme }) => ({
+  position: "absolute",
+  bottom: theme.spacing(2),
+  left: "50%",
+  transform: "translateX(-50%) translateY(-130%)",
+  display: "flex",
+  zIndex: "15",
+  gap: theme.spacing(2),
+}));
 const VideoCallModal = ({
   open,
   onClose,
@@ -42,86 +84,185 @@ const VideoCallModal = ({
   const [callEnded, setCallEnded] = useState(false);
   const [videoReady, setVideoReady] = useState(false);
   const [stream, setStream] = useState();
-  const myVideo = useRef();
-
-  const userVideo = useRef();
-
-  const peerConnectionRef = useRef();
+  const myVideo = useRef(null);
+  const [video, setVideo] = useState(null);
+  const userVideo = useRef(null);
+  const peerConnectionRef = useRef(null);
 
   useEffect(() => {
+    console.log(userVideo);
     const getMediaStream = async () => {
       try {
-        console.log("Requesting media stream...");
-        navigator.mediaDevices
-          .getUserMedia({ video: true, audio: true })
-          .then((stream) => {
-            setStream(stream);
-            myVideo.current.srcObject = stream;
-            console.log("myVideo :", myVideo.current.srcObject);
-            const callUser = () => {
-              const peerConnection = new RTCPeerConnection();
-              console.log("callUser");
-              if (stream) {
-                console.log("inside stream");
-                stream.getTracks().forEach((track) => {
-                  peerConnection.addTrack(track, stream);
-                });
-              }
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+        setStream(stream);
+        setStream(stream);
+        console.log(stream.getTracks(), "checking my video tracks");
+        if (myVideo.current) {
+          myVideo.current.srcObject = stream;
+        }
+        console.log("myVideo :", myVideo.current.srcObject);
 
-              peerConnection
-                .createOffer()
-                .then((offer) => {
-                  return peerConnection.setLocalDescription(offer);
-                })
-                .then(() => {
-                  socket.current.emit("callUser", {
-                    userToCall: receiverId,
-                    signalData: peerConnection.localDescription,
-                    from: me,
-                    name: name,
-                  });
-                });
+        const callUser = () => {
+          const peerConnection = new RTCPeerConnection();
+          console.log("callUser");
+          if (stream) {
+            stream.getTracks().forEach((track) => {
+              console.log("inside track", track);
 
-              socket.current.on("callAccepted", (signal) => {
-                console.log("call accepted");
-                setCallAccepted(true);
-                peerConnection.setRemoteDescription(
-                  new RTCSessionDescription(signal)
-                );
+              peerConnection.addTrack(track, stream);
+            });
+          }
+
+          peerConnection
+            .createOffer()
+            .then((offer) => {
+              return peerConnection.setLocalDescription(offer);
+            })
+            .then(() => {
+              socket.current.emit("callUser", {
+                userToCall: receiverId,
+                signalData: peerConnection.localDescription,
+                from: me,
+                name: name,
               });
+            });
 
-              peerConnection.ontrack = (event) => {
-                console.log("inside event track of callUser");
-                if (event.streams && event.streams[0]) {
-                  if (userVideo.current) {
-                    userVideo.current.srcObject = event.streams[0];
-                    console.log(
-                      "Received video track:",
-                      userVideo.current.srcObject
-                    );
-                  }
-                }
-              };
+          peerConnection.ontrack = (event) => {
+            console.log("inside event track of callUser");
 
-              peerConnectionRef.current = peerConnection;
-            };
+            if (event.streams && event.streams[0]) {
+              userVideo.current.srcObject = event.streams[0];
 
-            if (!isIncomingCall) {
-              callUser();
+              setVideo(event.streams[0]);
+
+              if (userVideo.current.srcObject) {
+                console.log(
+                  "userVideo checking callUser :",
+                  userVideo.current.srcObject
+                );
+              }
             }
-          })
-          .catch((error) => {
-            console.error("Error accessing user media:", error);
+          };
+
+          socket.current.on("callAccepted", (signal) => {
+            console.log("call accepted");
+            setCallAccepted(true);
+            peerConnection.setRemoteDescription(
+              new RTCSessionDescription(signal)
+            );
           });
+
+          peerConnectionRef.current = peerConnection;
+        };
+
+        if (!isIncomingCall) {
+          callUser();
+        }
       } catch (error) {
-        console.error("Error accessing media:", error);
+        console.error(error);
       }
     };
+
     if (!stream) {
       getMediaStream();
-      return;
     }
+
+    return () => {
+      if (peerConnectionRef.current) {
+        peerConnectionRef.current.close();
+      }
+
+      if (stream) {
+        stream.getTracks().forEach((track) => {
+          track.stop();
+        });
+
+        // Set srcObject to null to stop video playback
+        if (myVideo.current) {
+          myVideo.current.srcObject = null;
+        }
+      }
+    };
   }, []);
+
+  // useEffect(() => {
+  //   const getMediaStream = async () => {
+  //     try {
+  //       const stream = await navigator.mediaDevices.getUserMedia({
+  //         video: true,
+  //         audio: true,
+  //       });
+  //       setStream(stream);
+
+  //       if (myVideo.current) {
+  //         myVideo.current.srcObject = stream;
+  //       }
+
+  //       const peerConnection = new RTCPeerConnection();
+
+  //       if (stream) {
+  //         stream.getTracks().forEach((track) => {
+  //           peerConnection.addTrack(track, stream);
+  //         });
+  //       }
+
+  //       peerConnection
+  //         .createOffer()
+  //         .then((offer) => peerConnection.setLocalDescription(offer))
+  //         .then(() => {
+  //           socket.current.emit("callUser", {
+  //             userToCall: receiverId,
+  //             signalData: peerConnection.localDescription,
+  //             from: me,
+  //             name: name,
+  //           });
+  //         });
+
+  //       peerConnection.ontrack = (event) => {
+  //         if (event.streams && event.streams[0]) {
+  //           userVideo.current.srcObject = event.streams[0];
+  //           setVideo(event.streams[0]);
+  //         }
+  //       };
+
+  //       socket.current.on("callAccepted", (signal) => {
+  //         setCallAccepted(true);
+  //         peerConnection.setRemoteDescription(
+  //           new RTCSessionDescription(signal)
+  //         );
+  //       });
+
+  //       peerConnectionRef.current = peerConnection;
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   if (!stream) {
+  //     getMediaStream();
+  //   }
+
+  //   // Cleanup function
+  //   return () => {
+  //     if (peerConnectionRef.current) {
+  //       peerConnectionRef.current.close();
+  //     }
+
+  //     if (stream) {
+  //       stream.getTracks().forEach((track) => {
+  //         track.stop();
+  //       });
+
+  //       // Set srcObject to null to stop video playback
+  //       if (myVideo.current) {
+  //         myVideo.current.srcObject = null;
+  //       }
+  //     }
+  //   };
+  // }, []);
 
   const answerCall = (callerId, signalData) => {
     console.log("callerId:", callerId);
@@ -131,6 +272,7 @@ const VideoCallModal = ({
 
     if (stream) {
       stream.getTracks().forEach((track) => {
+        console.log("trackData", track);
         peerConnection.addTrack(track, stream);
       });
     }
@@ -148,16 +290,21 @@ const VideoCallModal = ({
           to: callerId,
         });
       });
-    setCallAccepted(true);
-
     peerConnection.ontrack = (event) => {
       console.log("inside event track of anserCall");
 
       if (event.streams && event.streams[0]) {
         userVideo.current.srcObject = event.streams[0];
-        console.log("userVideo checking :", userVideo.current.srcObject);
+        console.log(event.streams[0].getTracks(), "mediaStream");
+        setVideo(event.streams[0]);
+
+        if (userVideo.current.srcObject) {
+          console.log("userVideo checking :", userVideo.current.srcObject);
+        }
       }
     };
+
+    setCallAccepted(true);
 
     peerConnectionRef.current = peerConnection;
   };
@@ -178,8 +325,19 @@ const VideoCallModal = ({
   }, [socket.current, currentUser]);
 
   const handleCallEnd = () => {
+    console.log("inside cal end");
     setCallEnded(true);
     onClose();
+    if (stream) {
+      stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+
+      // Set srcObject to null to stop video playback
+      if (myVideo.current) {
+        myVideo.current.srcObject = null;
+      }
+    }
 
     if (peerConnectionRef.current) {
       peerConnectionRef.current.close();
@@ -188,6 +346,14 @@ const VideoCallModal = ({
 
     socket.current.emit("callEnded", { userId: receiverId });
   };
+
+  // useEffect(() => {
+  //   if (userVideo.current && callAccepted && !callEnded && video) {
+  //     userVideo.current.srcObject = video;
+  //   } else {
+  //     console.log("Video track is not available or not live.");
+  //   }
+  // }, [video, callAccepted, callEnded]);
 
   return (
     <StyledModal
@@ -201,56 +367,88 @@ const VideoCallModal = ({
           {isIncomingCall ? "Incoming Video Call" : "Ongoing Video Call"}
         </h2>
 
-        <div className="video">
+        <VideoContainer>
           {stream && (
             <>
               <video
                 playsInline
-                muted
                 ref={myVideo}
                 autoPlay
-                style={{ width: "300px", marginBottom: "1rem" }}
+                style={{
+                  width: callAccepted ? "150px" : "400px",
+                  marginBottom: "1rem",
+                  position: callAccepted && !callEnded ? "absolute" : "static",
+                  top: callAccepted && !callEnded ? 140 : "auto",
+                  right: callAccepted && !callEnded ? 130 : "auto",
+                  zIndex: callAccepted && !callEnded ? 1 : "auto",
+                  border: callAccepted && !callEnded ? "1px solid red" : "none",
+                  borderRadius: callAccepted && !callEnded ? "8px" : "0",
+                  boxShadow: callAccepted && !callEnded ? 5 : "none",
+                }}
               />
-
               {!isIncomingCall && !callAccepted && <h4>calling ....</h4>}
             </>
           )}
-        </div>
-
-        <div className="video">
-          {callAccepted && !callEnded && (
-            <video
-              playsInline
-              ref={userVideo}
-              autoPlay
-              style={{
-                width: "300px",
-                marginBottom: "1rem",
-                height: "200px",
-                border: "1px solid red",
-              }}
-            />
+          {isIncomingCall && !callAccepted && (
+            <CallerInfo>
+              <h1>{name} is calling...</h1>
+            </CallerInfo>
           )}
-        </div>
-
-        {isIncomingCall && !callAccepted && (
-          <div className="caller">
-            <h1>{name} is calling...</h1>
-            <Button
+          <ButtonContainer>
+            {isIncomingCall && !callAccepted && (
+              <StyledButton
+                variant="contained"
+                color="primary"
+                onClick={() => answerCall(caller, callerSignal)}
+              >
+                <PhoneIcon />
+              </StyledButton>
+            )}
+            <StyledButton
               variant="contained"
-              color="primary"
-              onClick={() => answerCall(caller, callerSignal)}
+              color="secondary"
+              onClick={handleCallEnd}
             >
-              Answer
-            </Button>
-          </div>
-        )}
+              <CallEndIcon />
+            </StyledButton>
+          </ButtonContainer>
+        </VideoContainer>
 
-        <div>
-          <Button variant="contained" color="secondary" onClick={handleCallEnd}>
-            End Call
-          </Button>
-        </div>
+        <VideoContainer>
+          {callAccepted && !callEnded && (
+            <>
+              <video
+                style={{
+                  width: "100%",
+                  height: "400px",
+                  maxWidth: "400px",
+                  border: "1px solid red",
+                  borderRadius: "8px",
+                }}
+                playsInline
+                ref={userVideo}
+                autoPlay
+                muted
+              />
+            </>
+          )}
+        </VideoContainer>
+
+        {/* <StyledButton
+          variant="contained"
+          color="primary"
+          onClick={() => answerCall(caller, callerSignal)}
+        >
+          <PhoneIcon />
+        </StyledButton>
+        <StyledButton
+          variant="contained"
+          color="secondary"
+          style={{ borderRadius: "50%", width: "40px" }}
+          onClick={handleCallEnd}
+        >
+          <CallEndIcon />
+        </StyledButton> */}
       </ModalContent>
     </StyledModal>
   );
